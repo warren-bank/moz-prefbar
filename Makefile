e293cc71f3d46875d42516affb4ee320a2e3c811
Manuel Reimer <manuel.reimer@gmx.de>
2011-07-29 15:22:33 +0200

    PrefBar 6.0.1

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@
 # Makefile for PrefBar
 #
 
-VERSION=6.0.0
-BUILD=20110708
+VERSION=6.0.1
+BUILD=20110728
 
 all: patch jar xpi
 
--- content/prefbar.js
+++ content/prefbar.js
@@ -41,7 +41,7 @@
 // |  goprefbar.js component (goPrefBar)
 // +-
 
-const prefbarVersion = 20110708;
+const prefbarVersion = 20110728;
 
 var PrefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 var PrefBranch = PrefService.getBranch("");
