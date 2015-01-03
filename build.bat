3b30ebbf5dd307ca9dc929008272d77e2bef9f6c
Warren R Bank <warren.r.bank@gmail.com>
2015-01-02 19:51:53 -0800

    add: LICENSE

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 rem :: http://sevenzip.sourceforge.jp/chm/cmdline/commands/add.htm
 
-7z a -tzip -scsUTF-8 "prefbar.xpi" ".\chrome.manifest" ".\install.rdf" "chrome\" "components\" "defaults\"
+7z a -tzip -scsUTF-8 "prefbar.xpi" ".\chrome.manifest" ".\install.rdf" ".\LICENSE" "chrome\" "components\" "defaults\"
